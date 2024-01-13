b60bd787dc779f82550a910994fd286e7de28cab
Herrison Féres <herrisonf_nascimento@hotmail.com>
2024-01-13 11:43:56 -0300

    Ajuste nos textos

--- lib/pages/home_page/components/introduction_resume_component.dart
+++ lib/pages/home_page/components/introduction_resume_component.dart
@@ -17,17 +17,29 @@ class IntroductionResumeComponent extends StatelessWidget {
         ],
       ),
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          Container(
+            margin: const EdgeInsets.only(bottom: 10),
+            child: Text(
+              "HISTÓRIA",
+              textAlign: TextAlign.justify,
+              style: GoogleFonts.montserrat(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
           Text(
-            " Iniciei minha carreira com desenvolvimento Delphi, onde criei soluções "
+            "Iniciei minha carreira com desenvolvimento Delphi, onde contribui para criar soluções "
             "para automações comerciais, refatorando e tendo vivências em Backend. "
-            "Apesar do inicio, meu foco atual é a plataforma Mobile. \n Na minha carreira, "
-            "já passei por empresas onde pude melhorar minhas habilidades pessoais, de desenvolvimento, "
+            " Apesar do inicio, meu foco atual é a plataforma Mobile. \n Na minha carreira, "
+            "já passei por empresas onde pude melhorar minhas habilidades pessoais, desenvolvimento, "
             "tato ao cliente final e trabalho em equipe. Já tive também a oportunidade de experiênciar"
-            "a liderança de uma equipe em um projeto. Em outras empresas, "
+            "a liderança de uma equipe em um projeto. \n Em outras empresas, "
             "assumi a responsabilidade para criar arquiteturas de softwares "
             "mobile e prototipações de aplicações para clientes finais. \n Nem todos os projetos que participei"
-            " estão aqui em imagens, pois se tratavam de aplicações mais internas que não vieram a público, "
+            "estão aqui em imagens, pois se tratavam de aplicações mais internas que não vieram a público em lojas, "
             "então opto por não expor.",
             textAlign: TextAlign.justify,
             style: GoogleFonts.montserrat(
@@ -48,7 +60,7 @@ class IntroductionResumeComponent extends StatelessWidget {
             height: 20,
           ),
           Text(
-            "React; Firebase, MVC, MVVP, MVP, SOLID e Arquitetura Limpa; Jenkins; Figma; Clean Code; Git/Flow, SCRUM, JIRA, LT. ",
+            "React - Firebase - MVC - MVVP - MVP - SOLID - Arquitetura Limpa - Jenkins; \nFigma - Clean Code - Git/Flow - SCRUM - JIRA - LT(Liderança Técnica). ",
             style: GoogleFonts.montserrat(
               fontSize: 18,
             ),
@@ -68,17 +80,17 @@ class IntroductionResumeComponent extends StatelessWidget {
             height: 20,
           ),
           Text(
-            "- Projeto White label MobileBus(Flutter), Checkin(Nativo), Agência Digital (Nativo), MovingTech(FLUTTER WEB/MOBILE) - TOTVS \n "
+            "- Projeto White label MobileBus(Flutter), Checkin(Nativo e FLUTTER), Agência Digital (Nativo), Agência Digital (Nativo), MovingTech(FLUTTER WEB e MOBILE) - TOTVS \n "
             "- Hospital Albert Einstein (Flutter) - Avanade \n "
             "- Supermercado Mambo Delivery (L.T, Flutter) - Accurate Software \n "
+            "- Aplicativo FONTE (Flutter) - Freelancer \n"
             "- Meu Técnico(Claro, Android) - Accurate Software \n "
             "- Portal LGPD(Claro, React) - Accurate Software \n "
-            "- Migração regionalizador OI(OI, Oracle Infrastructure Cloud) - Accurate Software \n "
-            "- Integração CIP(CIP, Oracle Cloud) - Accurate Software, \n entre outros. ",
+            "- Migração regionalizador OI(OI, Oracle Infrastructure Cloud) - Accurate Software entre outros. \n",
             style: GoogleFonts.montserrat(
               fontSize: 18,
             ),
-            textAlign: TextAlign.justify,
+            textAlign: TextAlign.left,
           ),
           const SizedBox(
             height: 20,
